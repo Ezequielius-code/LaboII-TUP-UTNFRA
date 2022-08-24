883296648454f480bf221d513c6f07f429905300
ezequiel <bruttoezequiel@gmail.com>
2022-08-23 21:34:50 -0300

    1stClass

---
+++ IntroducciónACSharp/EjercicioI03/LosPrimos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace LosPrimos
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string answer = "Y";
+
+            do
+            {
+                Console.WriteLine("To calculate prime numbers, enter a number.\n" +
+                                  "If you wish to close the app, enter 'N'.");
+                answer = Console.ReadLine();
+
+                if (int.TryParse(answer, out int enteredNumber))
+                {
+                    int temp = 0;
+                    for (int i = 2; i <= enteredNumber; i++)
+                    {
+                        for (int j = 1; j <= i; j++)
+                        {
+                            if (i % j == 0)
+                                temp++;
+                        }
+                        if (temp <= 2)
+                        {
+                            Console.WriteLine("{0}", i);
+                        }
+                        temp = 0;
+                    }
+
+                    Console.WriteLine("\n¿Do you want to enter a new number or close the app?\n   Enter: 'Y'es - 'N'o");
+                    answer = Console.ReadLine();
+                }
+            } while (answer == "Y");
+        }
+    }
+}
---
+++ IntroducciónACSharp/EjercicioI04/UnNumeroPerfecto/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UnNumeroPerfecto
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            /*
+            *To do this exercise, I won´t use the optimal algorythm.
+            *This is due to the ask of the first 4 perfect numbers
+            *is coincident with the fact that, the first 4 perfect numbers
+            *discovered and published have been proposed by Euclid in his
+            *highly-influential book "Elements" (c.300 bC). I wish to use a similar
+            *system to the proposed by this genius as a homage.
+            */
+
+            int temp = 1;
+            int addition = 0;
+            int aux = 0;
+            byte count = 0;
+
+            for (int i = 1; count < 4; i *= 2)
+            {
+                temp = i;
+                addition += i;
+                for (int lastNumber = 2; lastNumber < addition; lastNumber++)
+                {
+                    if (addition % lastNumber == 0)
+                    {
+                        aux++;
+                    }
+                }
+                if (aux == 0 && i != 1)
+                {
+                    int perfectNumber = temp * addition;
+                    Console.WriteLine("Perfect number: " + perfectNumber);
+                    count++;
+                }
+                aux = 0;
+            }
+        }
+    }
+}
